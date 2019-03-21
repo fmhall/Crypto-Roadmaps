55dae995e81b91daafa41e02a8db4af7e8ffb464
Mason Hall <masonhall@gmail.com>
2019-03-21 00:45:56 -0500

    Admin Panel for adding projects

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import Project
+from tinymce.widgets import TinyMCE
+from django.db import models
 # Register your models here.
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    fields = ["project_name",
+              "project_published",
+              "project_description"]
+
+    formfield_overrides = {
+        models.TextField: {'widget': TinyMCE()},
+    }
+
+
+admin.site.register(Project, ProjectAdmin)
--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=200)
+    project_description = models.TextField()
+    project_published = models.DateTimeField(
+        'date published', default=datetime.now)
+
+    def __str__(self):
+        return self.project_name
